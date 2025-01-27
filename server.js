aaddc1809e462c69503ceceaf0d98f2168566701
unknown <vickielewinsky@gmail.com>
2025-01-27 15:36:53 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+// Import necessary modules
+const express = require('express');
+const path = require('path');
+
+// Initialize the Express app
+const app = express();
+const PORT = 3000;
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the Home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Route for the About page
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+// Route for the Contact page
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
